b928760e24003869fad38d826a66dfbbcaf268af
fahmi nur rahman <fachminur@gmail.com>
2020-10-06 11:09:46 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\User;
+use DB;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'username' => 'required|string|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        try 
+        {
+            $user = new User;
+            $user->username= $request->input('username');
+            $user->password = app('hash')->make($request->input('password'));
+            $user->save();
+
+            return response()->json( [
+                        'entity' => 'users', 
+                        'action' => 'create', 
+                        'result' => 'success'
+            ], 201);
+
+        } 
+        catch (\Exception $e) 
+        {
+            return response()->json( [
+                       'entity' => 'users', 
+                       'action' => 'create', 
+                       'result' => 'failed'
+            ], 409);
+        }
+    }
+	
+     /**
+     * Get a JWT via given credentials.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 
+    public function login(Request $request)
+    {
+          //validate incoming request 
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('username',$request->input('username'))->first();
+        if (!$user) {
+            // You wil probably have some sort of helpers or whatever
+            // to make sure that you have the same response format for
+            // differents kind of responses. But let's return the 
+            // below respose for now.
+            return response()->json([
+                'code'    =>  401,
+                'message' => 'Username does not exist.'
+            ], 401);
+        }
+
+        $credentials = $request->only(['username', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {			
+            return response()->json([
+                'code'      =>  401,
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        $lastLogin = date('Y-m-d H:i:s');
+        DB::table('users')->where('username',$request->input('username'))->update(['token' => $token, 'last_login' => $lastLogin]);
+        return $this->respondWithToken($token);
+    }
+	
+     /**
+     * Get user details.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 	
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'code'      =>  401,
+            'message' => 'Successfully logged out'
+        ],401);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondWithToken($token)
+    {
+        return response()->json([
+            'code'      =>  200,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ],200);
+    }
+}
